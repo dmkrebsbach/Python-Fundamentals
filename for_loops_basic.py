ddaed164ee2d95e52695e74e7af6add742907ecc
dmkrebsbach <damiank@uw.edu>
2019-06-30 11:03:06 -0700

    first push for basic loops

---
+++ for_loops_basic.py
@@ -0,0 +1,55 @@
+#Basic
+# count = 0
+# while count <=150:
+#     print (count)
+#     count+=1
+
+#Print Multiples of 5 (For Loop and While Loop)
+# count = 0
+# for count in range (5,1005,5): 
+#     print (count)
+
+# count = 0
+# while count <=1000:
+#     print(count)
+#     count+=5
+
+#Counting the Dojo Way
+# count = 0
+# while count <=100:
+#     if count%10 == 0:
+#         print("Dojo")
+#         count+=1
+#     elif count%5 == 0:
+#         print("Coding")
+#         count+=1
+#     else:
+#         print(count)
+#         count+=1
+
+#Whoa that Sucker is huge
+# sum = 0
+# for count in range (0,500000):
+#     sum+=count
+# print(sum)
+
+#Countdown by Fours
+# count =2018
+# while count > 0:
+#     print(count)
+#     count-=4
+
+#Flexible Counter
+# def flexibleCounter(lowNum, highNum, mult):
+#     count = lowNum
+#     while count <= highNum:
+#         if count%mult == 0:
+#             print(count)
+#             count+=1
+#         else:
+#             count+=1
+
+# flexibleCounter(2,255,7)
+        
+
+

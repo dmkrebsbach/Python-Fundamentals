e152fa5d80a3671e3cc5df815b2107c623b83ffe
dmkrebsbach <damiank@uw.edu>
2019-06-30 11:55:56 -0700

    functions practice python

---
+++ functions_basic2.py
@@ -0,0 +1,55 @@
+# #Countdown
+# def countdown(num):
+#     while num >= 0:
+#         print(num)
+#         num-=1
+# countdown(25)
+
+
+
+# #Print and Return
+# def printAndReturn(num1, num2):
+#     print(num1)
+#     return(num2)
+
+# x = printAndReturn(35,90)
+# print(x)
+
+
+
+# # First Plus Length (accepts a list = array)
+# def firstPlusLength(list):
+#     return list[0]+len(list)
+
+# firstPlusLength([1,2,3,4,5,6])
+# print (firstPlusLength([1,2,3,4,5,6]))
+
+
+
+# # Values Greater Than Second
+# def valGTS(list):
+#     list2 = []
+#     count = 0
+#     for x in range (0,len(list),1):
+#        if list[x] > list[1]:
+#             list2.append(list[x])
+#             count+=1
+#     print(len(list2))
+#     print(list2)
+#     return list2
+#     if len(list) < 3:
+#         return False
+# valGTS([5,2,3,2,1,4])
+
+
+
+# This Length That Value
+
+# def length_and_value(num1, num2):
+#     newList=[]
+#     for x in range (0,num1,1):
+#         newList.append(num2)
+#     print(newList)
+#     return(newList)
+# length_and_value(4,7)
+# length_and_value(6,2)
